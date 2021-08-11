787f02e302dad70833a19931197ff7bb720c0f09
S460 <aleksandar.ta6ev@gmail.com>
2021-08-11 20:26:00 +0300

    add message model

--- models/User.js
+++ models/User.js
@@ -19,6 +19,10 @@ const UserSchema = new Schema({
 		required: [true, 'Please enter a password.'],
 		minlength: [6, 'Minimum password length is 6 characters.'],
 	},
+	status: {
+		type: String,
+		enum: ['admin', 'unknown', 'user'],
+	},
 });
 
 UserSchema.virtual('fullname').get(function () {
